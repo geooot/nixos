3c0a3c52110159597d5abf8e04c7dbe0c65bf90c
George Thayamkery <george.thayamkery@gmail.com>
2025-07-05 14:10:09 -0700

    update colors

--- stylix/configuration.nix
+++ stylix/configuration.nix
@@ -9,7 +9,7 @@
 }:
 {
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/primer-dark-dimmed.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
   stylix.polarity = "dark";
   stylix.fonts.monospace = {
     package = inputs.fontpkgs.packages.x86_64-linux.berkeley-mono;
