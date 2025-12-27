15bd86de5c2b34b120f5e63e70f7b3d6ecbe0d67
George Thayamkery <george.thayamkery@gmail.com>
2025-12-27 06:46:21 -0800

    format

--- niri/configuration.nix
+++ niri/configuration.nix
@@ -10,9 +10,25 @@
     package = pkgs.niri;
   };
 
-  xdg.portal.enable = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+    config = {
+      niri = {
+        default = [ "gnome" "gtk" ];
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+      };
+    };
+  };
 
   # Chromium apps have a flicker when using nvidia drivers
   # This apparently fixes it
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    # Ensure system portals are used instead of user profile portals
+    NIX_XDG_DESKTOP_PORTAL_DIR = "/run/current-system/sw/share/xdg-desktop-portal/portals";
+  };
 }
--- xremap/configuration.nix
+++ xremap/configuration.nix
@@ -18,30 +18,29 @@
     ];
     config.keymap = [
       {
-	name = "Copy Paste";
-	remap = { 
-	  "Super-c" = "C-KEY_INSERT";
-	  "Super-v" = "Shift-KEY_INSERT";
-	  "Super-a" = "C-a";
-	  "Super-z" = "C-z";
-	  "Super-b" = "C-b";
-	  "Super-d" = "C-d";
-	  "Super-u" = "C-u";
-	  "Super-i" = "C-i";
-	  "Super-s" = "C-s";
-	  "Super-r" = "C-r";
+        name = "Copy Paste";
+        remap = {
+          "Super-c" = "C-KEY_INSERT";
+          "Super-v" = "Shift-KEY_INSERT";
+          "Super-a" = "C-a";
+          "Super-z" = "C-z";
+          "Super-b" = "C-b";
+          "Super-d" = "C-d";
+          "Super-u" = "C-u";
+          "Super-i" = "C-i";
+          "Super-s" = "C-s";
+          "Super-r" = "C-r";
         };
       }
       {
-	name = "Tab management";
-	remap = { 
-	  "Super-w" = "C-w";
-	  "Super-t" = "C-t";
-	  "Super-n" = "C-n";
-	  "Super-f" = "C-f";
+        name = "Tab management";
+        remap = {
+          "Super-w" = "C-w";
+          "Super-t" = "C-t";
+          "Super-n" = "C-n";
+          "Super-f" = "C-f";
         };
       }
     ];
   };
 }
-
